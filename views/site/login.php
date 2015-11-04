c86e9c1827f0a4aea804031e6a5318e77d17dfd0
Franklin Paula <franklin.paula@share.com.ec>
2015-11-04 16:10:03 -0500

    login

--- views/site/login.php
+++ views/site/login.php
@@ -1,46 +1,46 @@
 <?php
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\LoginForm */
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <?php $form = ActiveForm::begin([
+<section id="registro-chaide" class="background-registro">
+    <div class="cont-titulos">
+        <h1>Inicia Sesión</h1>
+        <p>Ingresa a tu cuenta y compra de una fácil, rápida y segura </p>
+        <div class="separador-p"><img src="<?= URL::base() ?>/images/separador.svg"/></div>
+    </div>
+    <div class="cont-formulario">
+            <?php $form = ActiveForm::begin([
         'id' => 'login-form',
-        'options' => ['class' => 'form-horizontal'],
+        'options' => ['class' => ''],
         'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
+            'template' => "<div class=\"cont-campos\">{label}{input}{error}</div>",
+               'options' => [
+                            'tag'=>'div'
+                           
+                        ]
+            //'labelOptions' => ['class' => 'col-lg-1 control-label'],
         ],
     ]); ?>
+       
+            <?= $form->field($model, 'username')->input('email')->label('Usuario') ?>
 
-    <?= $form->field($model, 'username') ?>
 
-    <?= $form->field($model, 'password')->passwordInput() ?>
-
-    <?= $form->field($model, 'rememberMe', [
-        'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-    ])->checkbox() ?>
-
-    <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
-            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            <?= $form->field($model, 'password')->passwordInput()->label('Password') ?>
+     
+        <input type="submit" value="Iniciar Sesión"/>
+           <?php ActiveForm::end(); ?>
+        <div class="div-registro">
+        *Si no posees cuenta en chaide, <a href="#">Regístrate Aquí</a>
         </div>
-    </div>
 
-    <?php ActiveForm::end(); ?>
-
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
     </div>
-</div>
+</section>
+<!-- -->
