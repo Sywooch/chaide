c6e4abd728942bc08e39bbdb4d59adebf2bff06f
Franklin <franklin.paula@share.com.ec>
2016-01-21 17:00:28 -0500

    profile

---
+++ views/user/_address.php
@@ -0,0 +1,27 @@
+                <div class="cont-infocamposf2" id="formulario-direccion">
+                <h1>DIRECCIONES DE ENVÍO</h1>
+                <div class="cont-infocampos">
+                    <label>Ciudad:</label>
+                    <select>
+                        <option>Quito</option>
+                        <option>Guayaquil</option>
+                    </select>
+                </div>
+                <div class="cont-infocampos">
+                    <label>Calle 1:</label>
+                    <input type="text"/>
+                </div>
+                <div class="cont-infocampos">
+                    <label>Calle 2:</label>
+                    <input type="text"/>
+                </div>
+                <div class="cont-infocampos">
+                    <label>Sector:</label>
+                    <input type="text"/>
+                </div>
+                <div class="cont-infocampos">
+                    <label>Número:</label>
+                    <input type="text"/>
+                </div>
+                <input type="submit" value="Guardar" class="btn-submit"/> 
+            </div> 
