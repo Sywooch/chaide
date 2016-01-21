6e981362edf6d8982626b204afcfa82062c2c690
Pablo Martinez <pablo.martinez@share.com.ec>
2016-01-21 17:12:46 -0500

    no message

--- mail/confirm.php
+++ mail/confirm.php
@@ -5,12 +5,12 @@
           	<table width="100%" style="padding:20px 0 0 0;">
             	<tr>
                 	<td>&nbsp;</td>
-                    <td style="color:#006ab0; font-size:54px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; text-align:center; font-weight:700;"><?= $model->names ?></td>
+                    <td style="color:#005f92; font-size:54px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; text-align:center; font-weight:700;"><?= $model->names ?></td>
                     <td>&nbsp;</td>
                 </tr>
                 <tr>
                 	<td width="150px" >&nbsp;</td>
-                    <td width="400px" style="color:#FFF; font-size:24px; background-color:#ec686d; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; text-align:center; font-weight:100;">GRACIAS POR REGISTRARTE</td>
+                    <td width="400px" style="color:#FFF; font-size:24px; background-color:#434038; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; text-align:center; font-weight:100;">GRACIAS POR REGISTRARTE</td>
                     <td width="150px" >&nbsp;</td>
                 </tr>
             </table>
@@ -39,7 +39,7 @@
           <td><a href="#" target="_blank"><img src="http://shareecuador.com/mailing/chaide/chaide-reg2.jpg" width="700" style="display:block" alt=""/></a></td>
         </tr>
         <tr >
-            <td style="background-color:#006ab0; text-align:center; font-size:12px; color:#FFF; padding:0 0 15px 0; color:#FFFFFF;"><a href="http://www.chaide.com/" target="_blank" style="color:inherit; text-decoration:none;">www.chaide.com</a> - <a href="https://www.facebook.com/ChaideOficial" target="_blank" style="color:inherit; text-decoration:none;">www.facebook.com/ChaideOficial</a></td>
+            <td style="background-color:#005f92; text-align:center; font-size:12px; color:#FFF; padding:0 0 15px 0; color:#FFFFFF;"><a href="http://www.chaide.com/" target="_blank" style="color:inherit; text-decoration:none;">www.chaide.com</a> - <a href="https://www.facebook.com/ChaideOficial" target="_blank" style="color:inherit; text-decoration:none;">www.facebook.com/ChaideOficial</a></td>
         </tr>
       </tbody>
     </table>
